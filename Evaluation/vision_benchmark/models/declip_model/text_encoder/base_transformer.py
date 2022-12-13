2a0e320021a67201b7d6f145d7877aa7d2e781d0
prettyde <130490956+prettyde@users.noreply.github.com>
2022-12-13 19:08:11 +0000

    trivial change

--- Evaluation/vision_benchmark/models/declip_model/text_encoder/base_transformer.py
+++ Evaluation/vision_benchmark/models/declip_model/text_encoder/base_transformer.py
@@ -30,4 +30,51 @@ class ResidualAttentionBlock(nn.Module):
     def __init__(self, d_model: int, n_head: int, attn_mask: torch.Tensor = None, dropout: float = 0.):
         super().__init__()
 
-        self.attn = nn.MultiheadAttention(d_model, n_head, dropout=dro
+        self.attn = nn.MultiheadAttention(d_model, n_head, dropout=dropout)
+        self.ln_1 = LayerNorm(d_model)
+        self.mlp = nn.Sequential(OrderedDict([
+            ("c_fc", nn.Linear(d_model, d_model * 4)),
+            ("gelu", QuickGELU()),
+            # ("dropout_1", nn.Dropout(dropout)),
+            ("c_proj", nn.Linear(d_model * 4, d_model)),
+            # ("dropout_2", nn.Dropout(dropout))
+        ]))
+        self.ln_2 = LayerNorm(d_model)
+        self.attn_mask = attn_mask
+
+    def attention(self, x: torch.Tensor):
+        self.attn_mask = self.attn_mask.to(
+            dtype=x.dtype, device=x.device) if self.attn_mask is not None else None
+        return self.attn(x, x, x, need_weights=True, attn_mask=self.attn_mask)[0]
+
+    def forward(self, x: torch.Tensor):
+        x = x + self.attention(self.ln_1(x))
+        x = x + self.mlp(self.ln_2(x))
+        return x
+
+
+class Transformer(nn.Module):
+    def __init__(self, width: int, layers: int, heads: int, attn_mask: torch.Tensor = None, checkpoint: bool = False, dropout: float = 0., emb_dropout: float = 0.):
+        super().__init__()
+        self.width = width
+        self.layers = layers
+        self.checkpoint = checkpoint
+        self.dropout = nn.Dropout(emb_dropout)
+        self.resblocks = nn.Sequential(
+            *[ResidualAttentionBlock(width, heads, attn_mask, dropout=dropout) for _ in range(layers)])
+
+    def checkpoint_fwd(self, layer, input, segments=2):
+        """checkpoint forward"""
+        # Make sure that the input to checkpoint have requires_grad=True, so that
+        # the autograd can take care of the checkpointed part of model
+        if not input.requires_grad:
+            input = input.detach()
+            input.requires_grad = True
+        return checkpoint_sequential(layer, segments, input)
+
+    def forward(self, x: torch.Tensor):
+        x = self.dropout(x)
+        if self.checkpoint:
+            return self.checkpoint_fwd(self.resblocks, x, self.layers)
+        return self.resblocks(x)
+
---
+++ Evaluation/vision_benchmark/models/declip_model/utils/nnclr_modules/nn_memory_bank.py
@@ -0,0 +1,3 @@
+""" Nearest Neighbour Memory Bank Module """
+
+# Copyright (c) 2021. Lightly AG and its affiliate
