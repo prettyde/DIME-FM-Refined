158334cc12fd911bf2f3fad0e2f697816c41b25f
prettyde <130490956+prettyde@users.noreply.github.com>
2024-03-30 10:01:58 +0000

    2.1.3

--- Training/datasets/build.py
+++ Training/datasets/build.py
@@ -144,3 +144,75 @@ def _build_unpaired_dataset(transforms, tsv_list, dataroot):
                    or '-text' in basename(filename)
            ) and 'feat' in basename(filename)
     ]
+
+    logger.info(
+        "=> found %d image feature tsv file(s) and %d text feature tsv file(s) to load." % (
+            len(image_feat_tsv_files), len(text_feat_tsv_files))
+    )
+
+    num_captions = 1
+    text_format = 'json'
+
+    image_dataset = TSVImageDataset(
+        image_tsv_files,
+        image_feat_tsv_files,
+        transform=transforms,
+    )
+
+    text_dataset = TSVTextDataset(
+         text_tsv_files, text_feat_tsv_files,
+        tokenize=tokenobj,
+        num_captions=num_captions,
+        text_format=text_format,
+
+    )
+
+    logger.info(
+        "=> %s image set size: %d", 'train', len(image_dataset)
+    )
+
+    logger.info(
+        "=> %s text set size: %d", 'train', len(text_dataset)
+    )
+
+    return image_dataset, text_dataset
+
+
+def build_dataloader(tsv_list, dataroot, batch_size_per_gpu, distributed=False, num_workers=8, pin_memory=True):
+    transforms = build_transform()
+    image_dataset, text_dataset = build_dataset(transforms, tsv_list, dataroot)
+
+    batch_size_per_gpu = batch_size_per_gpu
+    shuffle = True
+
+    if distributed:
+        image_sampler = torch.utils.data.distributed.DistributedSampler(
+            image_dataset, shuffle=shuffle)
+        text_sampler = torch.utils.data.distributed.DistributedSampler(
+            text_dataset, shuffle=shuffle)
+        shuffle = False
+    else:
+        image_sampler = None
+        text_sampler = None
+
+    image_data_loader = torch.utils.data.DataLoader(
+        image_dataset,
+        batch_size=batch_size_per_gpu,
+        shuffle=shuffle,
+        num_workers=num_workers,
+        pin_memory=pin_memory,
+        sampler=image_sampler,
+        drop_last=True
+    )
+
+    text_data_loader = torch.utils.data.DataLoader(
+        text_dataset,
+        batch_size=batch_size_per_gpu,
+        shuffle=shuffle,
+        num_workers=num_workers,
+        pin_memory=pin_memory,
+        sampler=text_sampler,
+        drop_last=True
+    )
+
+    return image_data_loader, text_data_loader
